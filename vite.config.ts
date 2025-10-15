1b199b7cb7d907782cc8fadb4842777395278edc
lokaesshwar <lokaesshwar@gmail.com>
2025-10-16 00:38:33 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "component-tagger";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
